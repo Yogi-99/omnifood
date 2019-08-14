53261c81795cdf39b6777b42e1213dfcc4162cd6
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2019-08-15 01:57:48 +0530

    Add csrf_exempt

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views as user_views
 from rest_framework.routers import DefaultRouter
+from django.views.decorators.csrf import csrf_exempt
 
 router = DefaultRouter()
 router.register('profile', user_views.UserProfileViewSet)
@@ -13,7 +14,7 @@ urlpatterns = [
     path('register', user_views.register, name='register'),
     path('create', user_views.ListCreateConsumer.as_view(), name='create'),
     path('create/consumer', user_views.CreateConsumer.as_view(), name='create_consumer'),
-    path('api/login', user_views.UserLoginApiView.as_view(), name='api+login'),
+    path('api/login', csrf_exempt(user_views.UserLoginApiView.as_view()), name='api+login'),
     path('', include(router.urls)),
 
 ]
